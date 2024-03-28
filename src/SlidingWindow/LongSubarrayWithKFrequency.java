e5718368f09000ae0a720ff41e244b4ea16a8d83
Manojkumar00876 <108590038+Manojkumar00876@users.noreply.github.com>
2024-03-29 00:34:10 +0530

    "29.03.2024"

---
+++ src/SlidingWindow/LongSubarrayWithKFrequency.java
@@ -0,0 +1,30 @@
+package SlidingWindow;
+import java.util.*;
+public class LongSubarrayWithKFrequency {
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,2,1,2,1,2};
+        int k = 4;
+       maxSubarrayLength(nums, k);
+    }
+
+    public static int maxSubarrayLength(int[] nums, int k) {
+
+        int l=0;
+        HashMap <Integer,Integer> map = new HashMap<>();
+        int result=-1;
+        for(int i=0; i<nums.length; i++) {
+
+            int num = nums[i];
+
+            map.put(num , map.getOrDefault(num,0)+1);
+
+            while(map.get(num) > k && l<=i) {
+                map.put(nums[l] , map.get(nums[l])-1);
+                l++;
+            }
+            result=Math.max(result,i-l+1);
+
+        }
+        return result;
+    }
+}
